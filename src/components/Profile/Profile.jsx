bc82d44db2e47249d9d60460b2328848298fdd79
olekskhom <oleks.khom@gmail.com>
2023-10-06 18:51:49 +0300

    profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+import user from '../../data/user.json';
+import css from './Profile.module.css';
+
+// const user = user;
+
+export const Profile = () => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={user.avatar} alt={user.avatar} className={css.avatar} />
+        <p className={css.name}>{user.username}</p>
+        <p className={css.tag}>@{user.tag}</p>
+        <p className={css.location}>{user.location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers </span>
+          <span className={css.quantity}>{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views </span>
+          <span className={css.quantity}>{user.stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes </span>
+          <span className={css.quantity}>{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
